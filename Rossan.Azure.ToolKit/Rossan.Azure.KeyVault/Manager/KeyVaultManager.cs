689dcf3e960e0f9cb5a740c330a19cf00f5afca3
rossan-94 <raushanpal1194@gmail.com>
2024-07-20 12:21:48 +0000

    Update KeyVaultManager.cs

--- Rossan.Azure.ToolKit/Rossan.Azure.KeyVault/Manager/KeyVaultManager.cs
+++ Rossan.Azure.ToolKit/Rossan.Azure.KeyVault/Manager/KeyVaultManager.cs
@@ -6,11 +6,13 @@ namespace Rossan.Azure.KeyVault.Manager
 {
     public class KeyVaultManager : KeyVaultRepository
     {
+        // MSI    
         public KeyVaultManager(string dnsName) : base(dnsName)
         {
             _secretClient = new SecretClient(vaultUri: new Uri(dnsName), new DefaultAzureCredential());
         }
 
+        // Application Object
         public KeyVaultManager(string dnsName, string clientId, string clientSecret, string tenantId) : base(dnsName)
         {
             _secretClient = new SecretClient(vaultUri: new Uri(dnsName), new ClientSecretCredential(tenantId, clientId, clientSecret));
